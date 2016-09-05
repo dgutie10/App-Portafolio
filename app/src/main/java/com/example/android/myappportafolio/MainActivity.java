ab998d9d5b2a5bca467ee03b02b2243edf28780a
Diego Gutiérrez <Diego Gutirrez>
2016-09-04 21:07:04 -0400

    Project ready for submission 1

--- app/src/main/java/com/example/android/myappportafolio/MainActivity.java
+++ app/src/main/java/com/example/android/myappportafolio/MainActivity.java
@@ -1,7 +1,21 @@
 package com.example.android.myappportafolio;
 
+import android.content.Context;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +24,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void launchToast (View view){
+        Button button = (Button) view;
+
+        String buttonText = button.getText().toString().toLowerCase();
+
+        Context context = getApplicationContext();
+        CharSequence toastContent  = "This button will launch my "+buttonText+" app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context,toastContent,duration);
+        toast.show();
+
+
+    }
+
+
+//    public static class PlaceholderFragment extends Fragment {
+//
+//        public PlaceholderFragment() {
+//        }
+//
+//        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedBundleState) {
+//            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+//
+//            String[] appNames = {
+//                    "Popular Movies",
+//                    "Stock Hawk",
+//                    "Build it Bigger",
+//                    "Make your App Material",
+//                    "Go Ubiquitous",
+//                    "Capstone"
+//            };
+//
+//            ArrayList<String> appList = new ArrayList<String>(Arrays.asList(appNames));
+//
+//            ArrayAdapter<String> buttonList = new ArrayAdapter<String>(
+//                    getActivity(),
+//                    R.layout.list_item_name,
+//                    R.id.list_item_app_textview,
+//                    appList
+//            );
+//
+//            ListView listView = (ListView) rootView.findViewById(R.id.listview_app_name);
+//            listView.setAdapter(buttonList);
+//
+//
+//            return rootView;
+//        }
+//    }
 }
